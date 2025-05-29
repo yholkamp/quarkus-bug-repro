18fee3dec37ae5cc32ca0fc8f641db434e2d9fc6
Yorick Holkamp <yorick@nextpulse.nl>
2025-05-29 15:40:18 +0200

    Simplified scenario to only a generic exception type

--- src/main/java/org/acme/MyTroublingClass.java
+++ src/main/java/org/acme/MyTroublingClass.java
@@ -11,14 +11,10 @@ import java.util.function.Consumer;
 public class MyTroublingClass {
 
   /**
-   * A convenience function which manages the lifecycle of a handle and yields it to a callback
-   * for use by clients.
-   *
-   * @param consumer A callback which will receive an open Handle
    * @param <X> exception type thrown by the callback, if any.
    *
    * @throws X any exception thrown by the callback
    */
-  public <X extends Exception> void useHandle(final Consumer<X> consumer) throws X {
+  public <X extends Exception> void useHandle() throws X {
   }
 }
